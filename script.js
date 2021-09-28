cdcb24fd0a63bf575bc63b1a479c74fc748cd2f4
sirsavant <stanleyho619@gmail.com>
2021-09-28 14:52:11 -0400

    added files to repo

---
+++ script.js
@@ -0,0 +1,7 @@
+/*
+
+JSON - a text-based data format file which has the same syntax compared to javascript objects where data is stored in property-value pairs and arrays. It is mainly used to transfer data in web applications. JSON exists as a string and when you need to use JSON, you need to deserialize it to use it in your program. When you need to convert it back to a string, you serialize it. JavaScript has a global object called JSON that provides methods for you to modify JSON.
+
+*/
+
+
