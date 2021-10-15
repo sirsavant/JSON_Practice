58ed25f7f6a5988a770be3c85c3fde7e167c05ec
sirsavant <stanleyho619@gmail.com>
2021-10-15 09:00:51 -0400

    add/update files

--- script.js
+++ script.js
@@ -4,4 +4,38 @@ JSON - a text-based data format file which has the same syntax compared to javas
 
 */
 
+const selectHeader = document.querySelector("header");
+const selectSection = document.querySelector("section");
 
+
+let requestURL = "https://sirsavant.github.io/JSON_Practice/file.json";
+
+let request = new XMLHttpRequest();
+
+// opens request
+request.open("GET", requestURL);
+
+// sets response type json so XHR knows that the server will be returning json.
+request.responseType = "json";
+request.send();
+
+// Once the request is received and loaded, the function executes. We are storing the response in a variable called occupations and calling two other functions.
+request.onload = function() {
+  const occupations = request.response;
+  populateHeader(occupations);
+  showOccupations(occupations);
+}
+
+function populateHeader(obj) {
+  let title = document.createElement("h1");
+  title.textContent = obj["Title"];
+  selectHeader.appendChild(title);
+
+  let subHeader = document.createElement("h2");
+  subHeader.textContent = `Location: ${obj["Location"]} // Date: ${obj["Date"]}`;
+  selectHeader.appendChild(subHeader);
+}
+
+function showOccupations(obj) {
+
+}
